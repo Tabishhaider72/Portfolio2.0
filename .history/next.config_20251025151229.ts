6e8afaf798697ef5d1f2320349d58c6cc8c5d3f5
SayedTabish72 <sayedtabish72@gmail.com>
2026-02-28 14:01:12 +0530

    refactor: remove turbopack configuration and retain image optimization settings

--- .history/next.config_20251025151229.ts
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-    /* config options here */
-    turbopack: {
-        // force Turbopack to use this package root (prevents Next from inferring C:\\Users\\Sayed as root)
-        root: './'
-    }
-};
-
-export default nextConfig;
